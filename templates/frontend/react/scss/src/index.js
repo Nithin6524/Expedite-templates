7c7bf829d5db1626525ebfb974c9d90bd6fe7f11
M SAI NITHIN <99406663+Nithin6524@users.noreply.github.com>
2025-05-28 21:21:45 +0530

    Update index.js

--- templates/frontend/react/scss/src/index.js
+++ templates/frontend/react/scss/src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
-import "./custom.scss";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
